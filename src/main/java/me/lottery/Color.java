db5f88fc6f18296da902c363b4639390f6ddc12c
kimpro12 <95573884+kimpro12@users.noreply.github.com>
2022-12-27 07:42:58 -0800

    Delete Color.java

--- src/main/java/me/lottery/Color.java
+++
@@ -1,9 +0,0 @@
-package me.lottery;
-
-import org.bukkit.ChatColor;
-
-public class Color {
-    public static String transalate(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-}
